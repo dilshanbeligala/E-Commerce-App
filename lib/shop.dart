475d25bab7b081680469233dd61f1957f978f169
dilshanbeligala <beligaladilshan@gmail.com>
2023-03-16 16:11:37 +0530

    shop page DONE

--- lib/shop.dart
+++ lib/shop.dart
@@ -12,121 +12,162 @@ class _ShopState extends State<Shop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 500,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/background.jpg'),
-                    fit: BoxFit.cover)),
-            child: Container(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 500,
               decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      colors: [
-                    Colors.black.withOpacity(.8),
-                    Colors.black.withOpacity(.2)
-                  ])),
-              child: Padding(
-                padding: EdgeInsets.only(top: 50.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.favorite,
-                              color: Colors.white,
-                            )),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.shopping_cart,
-                              color: Colors.white,
-                            ))
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Our New Products",
-                              style: GoogleFonts.orbitron(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white)),
-                          SizedBox(height: 20),
-                          Row(
-                            children: [
-                              Text(
-                                "VIEW MORE",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                              SizedBox(width: 5),
-                              Icon(
-                                Icons.arrow_forward_ios,
-                                size: 15,
+                  image: DecorationImage(
+                      image: AssetImage('images/background.jpg'),
+                      fit: BoxFit.cover)),
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.2)
+                    ])),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.favorite,
                                 color: Colors.white,
-                              )
-                            ],
-                          ),
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.shopping_cart,
+                                color: Colors.white,
+                              ))
                         ],
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Our New Products",
+                                style: GoogleFonts.orbitron(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            SizedBox(height: 20),
+                            Row(
+                              children: [
+                                Text(
+                                  "VIEW MORE",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                SizedBox(width: 5),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 15,
+                                  color: Colors.white,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Categories",
-                      style: GoogleFonts.inter(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    Text(
-                      "All",
-                      style: GoogleFonts.inter(),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 150,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      makeCategory(Image: 'images/beauty.jpg', title: 'Beauty'),
-                      makeCategory(
-                          Image: 'images/clothes.jpg', title: 'Clothes'),
-                      makeCategory(
-                          Image: 'images/perfume.jpg', title: 'Perfume'),
-                      makeCategory(Image: 'images/glass.jpg', title: 'Glass'),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Categories",
+                        style: GoogleFonts.inter(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        "All",
+                        style: GoogleFonts.inter(),
+                      )
                     ],
                   ),
-                )
-              ],
-            ),
-          )
-        ],
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 150,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        makeCategory(
+                            Image: 'images/beauty.jpg', title: 'Beauty'),
+                        makeCategory(
+                            Image: 'images/clothes.jpg', title: 'Clothes'),
+                        makeCategory(
+                            Image: 'images/perfume.jpg', title: 'Perfume'),
+                        makeCategory(Image: 'images/glass.jpg', title: 'Glass'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Best Selling by Category",
+                        style: GoogleFonts.inter(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        "All",
+                        style: GoogleFonts.inter(),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 150,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        makeBestCategory(
+                            Image: 'images/tech.jpg', title: 'Beauty'),
+                        makeBestCategory(
+                            Image: 'images/watch.jpg', title: 'Clothes'),
+                        makeBestCategory(
+                            Image: 'images/perfume.jpg', title: 'Perfume'),
+                        makeBestCategory(
+                            Image: 'images/glass.jpg', title: 'Glass'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -134,6 +175,38 @@ class _ShopState extends State<Shop> {
   Widget makeCategory({Image, title}) {
     return AspectRatio(
       aspectRatio: 2 / 2.5,
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            image:
+                DecorationImage(image: AssetImage(Image), fit: BoxFit.cover)),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.0)
+              ])),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              title,
+              style: GoogleFonts.inter(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeBestCategory({Image, title}) {
+    return AspectRatio(
+      aspectRatio: 3 / 2.5,
       child: Container(
         margin: EdgeInsets.only(right: 20),
         decoration: BoxDecoration(
