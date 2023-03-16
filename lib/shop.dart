b9095bd741c0a1cf3de809f5dc565266db236512
dilshanbeligala <beligaladilshan@gmail.com>
2023-03-16 11:41:29 +0530

    shop page list view DONE

--- lib/shop.dart
+++ lib/shop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Shop extends StatefulWidget {
   const Shop({super.key});
@@ -50,12 +51,114 @@ class _ShopState extends State<Shop> {
                             ))
                       ],
                     ),
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Our New Products",
+                              style: GoogleFonts.orbitron(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white)),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Text(
+                                "VIEW MORE",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              SizedBox(width: 5),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 15,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
             ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Categories",
+                      style: GoogleFonts.inter(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      "All",
+                      style: GoogleFonts.inter(),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      makeCategory(Image: 'images/beauty.jpg', title: 'Beauty'),
+                      makeCategory(
+                          Image: 'images/clothes.jpg', title: 'Clothes'),
+                      makeCategory(
+                          Image: 'images/perfume.jpg', title: 'Perfume'),
+                      makeCategory(Image: 'images/glass.jpg', title: 'Glass'),
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget makeCategory({Image, title}) {
+    return AspectRatio(
+      aspectRatio: 2 / 2.5,
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            image:
+                DecorationImage(image: AssetImage(Image), fit: BoxFit.cover)),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.0)
+              ])),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              title,
+              style: GoogleFonts.inter(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ),
       ),
     );
   }
